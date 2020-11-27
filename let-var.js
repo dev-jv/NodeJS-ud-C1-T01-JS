2513a882f8a355dd875d5973cc71509870dfa072
dev-jv <dev.javierleon@gmail.com>
2020-11-27 09:01:26 -0500

    one

---
+++ let-var.js
@@ -0,0 +1,20 @@
+var nombre = 'Kaede';
+
+if(true) {
+    var nombre = 'Deshojo';
+}
+
+console.log(nombre);
+
+
+
+let i = 'Red';
+
+for ( let i = 0; i <= 5; i++ ) {
+    console.log(`i: ${ i }`);
+}
+
+console.log(i);
+
+
+
